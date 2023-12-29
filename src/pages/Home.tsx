9180e7a5a7e2bf4c62024601aabcd4dd166589fa
Vishal Tiwari <vishaltiwari1532004@gmail.com>
2023-12-29 23:13:35 +0530

    Home page modification

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,41 @@
-type Props = {};
+import React, { useEffect } from "react";
+import { Button } from "@mui/material";
+import { useAppDispatch, useAppSelector } from "../hooks/reduxHooks";
+import { getUser, logout } from "../Slices/authSlice";
+import { useNavigate } from "react-router-dom";
 
-export default function Home({}: Props) {
-  return <div>Home</div>;
-}
+const Home = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const basicUserInfo = useAppSelector((state) => state.auth.basicUserInfo);
+  const userProfileInfo = useAppSelector((state) => state.auth.userProfileData);
+
+  useEffect(() => {
+    if (basicUserInfo) {
+      dispatch(getUser(basicUserInfo.id));
+    }
+  }, [basicUserInfo]);
+
+  const handleLogout = async () => {
+    try {
+      await dispatch(logout()).unwrap();
+      navigate("/login");
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <>
+      <h1>Home</h1>
+      <h4>Name: {userProfileInfo?.name}</h4>
+      <h4>Email: {userProfileInfo?.email}</h4>
+      <Button variant="contained" sx={{ mt: 3, mb: 2 }} onClick={handleLogout}>
+        Logout
+      </Button>
+    </>
+  );
+};
+
+export default Home;
